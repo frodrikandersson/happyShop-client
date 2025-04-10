61da9e2439e62194b49829907c7ed6dc1b7ff6d2
Fredrik Andersson <frodrikandersson@gmail.com>
2025-04-10 21:33:58 +0200

    test3

--- src/services/searchService.ts
+++ src/services/searchService.ts
@@ -3,7 +3,7 @@ import { IGoogleSearch } from "../models/IGoogleSearch";
 
 export async function googleSearch(query: string, start: number = 1) {
     try {
-      const url = `https://www.googleapis.com/customsearch/v1?q=${query}&start=${start}&key=${process.env.GOOGLE_API_SECRET}&cx=${process.env.GOOGLE_SEARCH_ENGINE_ID}`;
+      const url = `https://www.googleapis.com/customsearch/v1?q=${query}&start=${start}&key=${process.env.VITE_GOOGLE_API_SECRET}&cx=${process.env.VITE_GOOGLE_SEARCH_ENGINE_ID}`;
       const response = await axios.get(url)
       console.log("Google search service fetched successfully: ", response);
       return response.data as IGoogleSearch;
