3303cc994e8534f25667d87b7691f879f91e0376
Fredrik Andersson <frodrikandersson@gmail.com>
2025-04-11 03:24:39 +0200

    test3

--- src/services/searchService.ts
+++ src/services/searchService.ts
@@ -3,11 +3,13 @@ import { IGoogleSearch } from "../models/IGoogleSearch";
 
 export async function googleSearch(query: string, start: number = 1) {
     try {
+      console.log('API Key:', process.env.VITE_GOOGLE_API_SECRET);
+      console.log('Search Engine ID:', process.env.VITE_GOOGLE_SEARCH_ENGINE_ID);
       const response = await axios.get("https://www.googleapis.com/customsearch/v1", {
         params: {
           q:  query, start,
-          key: process.env.VITE_GOOGLE_API_SECRET,
-          cx: process.env.VITE_GOOGLE_SEARCH_ENGINE_ID,
+          key: "AIzaSyAjPePCnWnx-3Ou6R0nrZl1UyANWdnG6Ig",
+          cx: "c6bba88bd408443f0",
         }
       });
       console.log("Google search service fetched successfully: ", response);
